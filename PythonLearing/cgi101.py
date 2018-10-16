ffd118ef1f8fdefd8b9f4a6a25e64380f72930b2
guanenxi <guanenxi@outlook.com>
2018-10-16 23:21:55 +0800

    guanenxi@outlook.com

---
+++ PythonLearing/cgi101.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 17 16:33:37 2017
+
+@author: GuanEnxi
+"""
+
+import cgi
+
+form = cgi.FieldStorage()
+print('Content-type: text/html\n')
+print('<title>Reply Page</title>') 
+if not 'user' in form:
+    print('<h1>Who are you?</h1>')
+else:
+    print('<h1>Hello <i>%s</i>!</h1>' % cgi.escape(form['user'].value))
---
+++ PythonLearing/child.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 22 21:14:17 2018
+
+@author: GuanEnxi
+"""
+
+import os, sys
+print('Hello from child', os.getpid(), sys.argv[1])
